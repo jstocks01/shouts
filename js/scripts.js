9d34353ceaf2b73314e01197da49ecc7c41e0613
Student <example@example.com>
2020-01-15 10:22:59 -0800

    create chatbox, give function to a and b button.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $("#btn-a").click(function(){
+    $("#textbox").append("<li>Hello!</li>");
+  });
+    $("#btn-b").click(function(){
+      $("#textbox").append("<li>Goodbye</li>");
+    });
+
+  });
